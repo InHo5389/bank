85909f7b74e75e10894e73abed5b1b1ca03e9050
JEONG <qheogus55@naver.com>
2024-08-18 10:14:12 +0900

    create entity(account,user,transaction)

---
+++ src/main/java/bank/domain/transaction/Transaction.java
@@ -0,0 +1,55 @@
+package bank.domain.transaction;
+
+import bank.domain.account.Account;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "transaction_td")
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Account withdrawAccount;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Account depositAccount;
+
+    @Column(nullable = false)
+    private Long amount;
+
+    private Long withdrawAccountBalance;
+    private Long depositAccountBalance;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TransactionEnum status;
+
+    private String sender;
+    private String receiver;
+    private String tel;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+}
